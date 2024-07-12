0d1f5ea3dbddf69eb57437cb1f5ff83756497189
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-07-11 21:16:06 -0400

    windows faled,

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,6 @@ using namespace geode::prelude;
 $on_mod(Loaded) {
     CCFileUtils::get()->addTexturePack(CCTexturePack {
         .m_id = Mod::get()->getID(), // they're the same ID so it doesnt matter
-        .m_paths = { Mod::get()->getResourcesDir() }
+        .m_paths = { Mod::get()->getResourcesDir().string() }
     });
 }
